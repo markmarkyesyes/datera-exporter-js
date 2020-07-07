3f0bb80053ae8a353666e79f5cfb4d1398892e9a
markmarkyesyes <markhahn9@gmail.com>
2020-07-07 10:57:50 -0500

    perf: refactor metrics route to its own file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 8080
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/metrics', (req, res) => res.send('I am alive.'))
+
+const metricsRouter = require('./route-metrics');
+app.use('/metrics', metricsRouter)
+
 export {app}
